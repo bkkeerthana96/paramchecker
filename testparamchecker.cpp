be23731f93a532462b466d36bc0e9b44167bdf2d
bkkeerthana96 <68589444+bkkeerthana96@users.noreply.github.com>
2020-07-30 16:50:08 +0530

    Update testparamchecker.cpp

--- testparamchecker.cpp
+++ testparamchecker.cpp
@@ -18,6 +18,16 @@ TEST(VitalsTest, when_a_vital_is_off_limit_it_is_reported_with_vital_id) {
     ASSERT_EQ(results[bpm], true);
     ASSERT_EQ(results[respRate], true);
 }
+
+TEST(VitalsTest, when_spo2_is_low_and_respRate_is_high){
+    std::vector<Measurement> measurements = {
+     {spo2, 50},
+     {respRate, 80},
+    },
+    auto results = vitalsAreOk(measurements);
+    ASSERT_EQ(results.size(), sizeof(measurements)/sizeof(measurements[2]));
+    (ASSERT_EQ(results[spo2], false) && ASSERT_EQ(results[respRate], false)) << "Resprate and spo2 is high" << std::endl;
+}
  
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
